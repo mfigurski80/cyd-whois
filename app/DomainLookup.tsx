4e1ed2b7b4a7d3e681dd29ae69901c24412a2ee0
Mikolaj Figurski <meek.f80@gmail.com>
2023-02-03 13:13:40 -0500

    Add basic layout for actual lookup result

--- app/DomainLookup.tsx
+++ app/DomainLookup.tsx
@@ -6,11 +6,12 @@ import lookupQuery from "./lookup.graphql"
 import styles from './page.module.scss'
 
 import UrlForm from "./UrlForm"
+import ApolloQueryWrapper from "../components/ApolloQueryWrapper"
 
 export default function DomainLookup() {
   const router = useRouter()
   const params = useSearchParams()
-  const { loading, error, data } = useQuery(
+  const q = useQuery(
     gql`${lookupQuery}`, { 
       variables: { domain: params.get('lookup') }, 
       skip: !params.has('lookup')
@@ -28,11 +29,24 @@ export default function DomainLookup() {
         <UrlForm onSubmit={handleSubmit} />
       </div>
       <div className={styles.output}>
-        {loading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        {data && (
-          <pre>{JSON.stringify(data['domainLookup'], null, 2)}</pre>
-        )}
+        <ApolloQueryWrapper query={q}>
+          {({domainLookup: data}) => (
+            <div>
+              <h2>Domain Information for: <a href={'http://' + data.domain}>{data.domain}</a></h2>
+              <p>Status: {data.status}</p>
+              <p>Date: {data.create_date} (created) / {data.update_date} (updated)</p>
+              <p>Whois Server: {data.whois_server}</p>
+              <h3>Registrant</h3>
+              <p>Name: {data.registrant.name}</p>
+              <p>Organization: {data.registrant.organization}</p>
+              <p>Country: {data.registrant.country}</p>
+              <p>Email: {data.registrant.email}</p>
+              <h3>Origin Registrar</h3>
+              <p>Name: {data.registrar.name}</p>
+              <p>URL: <a href={data.registrar.url}>{data.registrar.url}</a></p>
+            </div>
+          )}
+        </ApolloQueryWrapper>
       </div>
     </main>
   )
