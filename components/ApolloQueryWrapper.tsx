31eec9962bda366a32e1a62fbfb437645e885cdf
Mikolaj Figurski <meek.f80@gmail.com>
2023-02-03 14:57:39 -0500

    Refactor error warning

--- components/ApolloQueryWrapper.tsx
+++ components/ApolloQueryWrapper.tsx
@@ -9,15 +9,20 @@ interface ApolloQueryWrapperProps {
 
 export default function ApolloQueryWrapper({ query, children }: ApolloQueryWrapperProps) {
   const { loading, error, data } = query
-  if (error) console.error(error)
+  if (error) console.error(error.graphQLErrors[0])
   return (
     <div>
       {loading ? (
         <p className={styles.loading}>Loading...</p>
       ) : error ? (
-        <p className={styles.error}>
-          <AiOutlineWarning /> Error: {error.message}
-        </p>
+        <div>
+          <p className={styles.error}>
+            <AiOutlineWarning /> Error {error.message} :: {error.graphQLErrors[0].extensions.response.body.error.error_message}
+          </p>
+          {error.graphQLErrors.map(({ extensions }:any, i:any) => (
+            <p className={styles.loading} key={i}>{JSON.stringify(extensions.response, null, 2)}</p>
+          ))}
+        </div>
       ) : null}
       {data && children(data)}
     </div>
